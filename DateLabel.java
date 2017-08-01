8933ae8d30fb3803659a5a30496d598f363458ab
Bryan Sugiarto <bryan.sugiarto@bryansugiar-ltm.internal.salesforce.com>
2017-08-01 11:35:36 -0700

    clean up datelabel class and remove unnecessary code

--- DateLabel.java
+++ DateLabel.java
@@ -2,65 +2,53 @@ import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.List;
 
 public class DateLabel {
 	
+	static boolean useToday = true; // enter useToday?
 	
 	static Date today = Calendar.getInstance().getTime();
-	static boolean useToday = true; // enter useToday?
 	static int month = 3; 	//enter month
 	static int day = 31; 	//enter day
 	static int year = 16; 	//enter year
+	static int dayOfWeek = 	3;
 	
-	static String [] weekday = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-	//							 0 		   1		  2 		   3		   4		 5			 6
-	static int daystart = 	3;
-	
-	static HashSet<Integer> longMonth = new HashSet<Integer>();
-	static Integer[] longMonths = {1, 3, 5, 7, 8, 10, 12};
-	
+	static final String [] weekdayArray = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+										//0 		   1		  		2 		   3		   		4		 5			 6
+	static final Integer[] longMonths = {1, 3, 5, 7, 8, 10, 12};
+	static HashSet<Integer> longMonth = new HashSet<Integer>(Arrays.asList(longMonths));
 
+	
 	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
-		
-		List<Integer> list =  Arrays.asList(longMonths);
-		longMonth.addAll(list);
-		
 		if(useToday) {
+			/* get current dates */
 			month = today.getMonth() + 1;
 			year = today.getYear() - 100;
-			daystart = today.getDay() - 1;
+			dayOfWeek = today.getDay() - 1;
 			day = today.getDate();
 			
-			//set daystart to the 28th day
-			while(day < 28) {
-				day++;
-				daystart++;
-			}
+			/* get the right weekdayArray for 28th of month*/
+			int diff = 28 - day;
+			day = 28;
+			dayOfWeek += diff;
 			
-			//make sure day is at 28 in order to proceed
-			if(day == 28){
-				//long month
-				if(longMonth.contains(month)) {
-					day	+= 3;
-					daystart += 3;
-				}
-				//short month not February
-				else if(month != 2) {
-					day += 2;
-					daystart +=2;
-				}
+			/* long month */
+			if(longMonth.contains(month)) {
+				day	+= 3;
+				dayOfWeek += 3;
 			}
-			daystart %= 7;
+			/* short month not February */
+			else if(month != 2) {
+				day += 2;
+				dayOfWeek +=2;
+			}
+			dayOfWeek += 700;
 		}
 		
 		/* print out the days */
 		while(day > 0) {
-			System.out.println(month + "/" + day-- + "/" + year + " " + weekday[daystart--]);
-			if(daystart < 0)
-				daystart = 6;
+			System.out.println(month + "/" + day-- + "/" + year + " " + weekdayArray[dayOfWeek-- % 7]);
 		}
 	}
-
 }
